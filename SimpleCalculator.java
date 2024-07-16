83c385419bd4bc95a60602b2ef583987f1bd9927
01fe22bca205 <01fe22bca205@kletech.ac.in>
2024-07-16 09:19:54 +0530

    program to cal

---
+++ SimpleCalculator.java
@@ -0,0 +1,30 @@
+public class SimpleCalculator {
+    public static void main(String[] args) {
+        double a = 5;
+        double b = 3;
+        
+        System.out.println("Addition: " + add(a, b));
+        System.out.println("Subtraction: " + subtract(a, b));
+        System.out.println("Multiplication: " + multiply(a, b));
+        System.out.println("Division: " + divide(a, b));
+    }
+
+    public static double add(double a, double b) {
+        return a + b;
+    }
+
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+
+    public static double divide(double a, double b) {
+        if (b == 0) {
+            throw new IllegalArgumentException("Division by zero is not allowed.");
+        }
+        return a / b;
+    }
+}
